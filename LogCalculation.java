78d2424895246fad8e2855d6c74a90712b2d9572
Bradley Wills <bradley.wills@mths.ca>
2020-02-16 00:23:47 +0000

    Third Commit

--- LogCalculation.java
+++ LogCalculation.java
@@ -2,7 +2,8 @@
 import java.util.Scanner;
 
 /**
-* The LogCalculation program implements an application that calculates how many maple logs should be placed on a truck if you know the lengths of the logs.
+* The LogCalculation program implements an application that calculates how many 
+* maple logs should be placed on a truck if you know the lengths of the logs.
 *
 * @author  Bradley Wills
 * @version 1.0
@@ -10,12 +11,15 @@ import java.util.Scanner;
 */
 
 public class LogCalculation {
-	// Create variables 
+  // Create variables 
   static double weight;
   static double logLength;
   static int logNumber;
   
-  // Calculates and outputs the amount of logs the truck can carry given the inputed log length
+  /**
+   * Calculates and outputs the amount of logs the truck can carry given the inputed log length.
+   */
+   
   public static void main(String[] args) {
     // Create scanner also found at https://www.programiz.com/java-programming/basic-input-output
     Scanner userInput = new Scanner(System.in);
@@ -24,16 +28,17 @@ public class LogCalculation {
     System.out.print("Please enter 0.25, 0.5, or 1: ");
     // Assign user input as logLength
     logLength = userInput.nextDouble();
-    if (logLength == 0.25||logLength == 0.5||logLength == 1) {
+    // Checks if input is valid
+    if (logLength == 0.25 || logLength == 0.5 || logLength == 1) {
       // Calculate the weight per log
       weight =  logLength * 20;
       // Calculate the amount of logs the truck can hold
       logNumber =  1100 / (int) weight;
       // Tells the user how many logs the truck can carry and restates the length of the log
-      System.out.println("The truck can carry " + logNumber + " logs that are " 
-        + logLength + "m long.");
+      System.out.println("The truck can carry " + logNumber + " " + logLength + " meter logs.");
     } else {
-    System.out.println("Invalid Input");
+      // Informs user their input is invalid
+      System.out.println("Invalid Input");
     }
   }
 }
